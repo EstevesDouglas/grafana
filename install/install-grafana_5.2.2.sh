713d96f1105f33cb02b703b40eaa6f3c033e082b
Douglas Esteves <douglas.brsoftware@gmail.com>
2018-08-14 22:50:28 -0300

    Create install-grafana_5.2.2.sh

---
+++ install/install-grafana_5.2.2.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Install Grafana 5.2.2 for CentOS 7
+# Author:      Douglas Esteves 
+# date:        15-August-2018
+
+# CentOS 7 & Redhat
+yum install -y https://s3-us-west-2.amazonaws.com/grafana-releases/release/grafana-5.2.2-1.x86_64.rpm   
+
+# Start service Grafana
+service grafana-server start
+
+#
+/sbin/chkconfig -add grafana-server
+
+#  start service automatically at boot 
+systemctl enable grafana-server
+
+# list many plugins
+grafana-cli plugins list-remote
+
+# Install plugin to zabbix in grafana
+grafana-cli plugins install alexanderzobnin-zabbix-app
+
+# Restart service Grafana
+service grafana-server restart
