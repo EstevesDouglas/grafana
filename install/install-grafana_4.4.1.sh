108ace39fae5d2d121783df366586b21a6d31768
Douglas Esteves <douglas.brsoftware@gmail.com>
2019-10-03 11:21:45 -0300

    Create install-grafana_4.4.1.sh
    grafana-6.4.1-1.x86_64.rpm

---
+++ install/install-grafana_4.4.1.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Install Grafana 6.4.1 for CentOS 7
+# Author:      Douglas Esteves 
+# date:        03-October-2019
+
+# CentOS 7 & Redhat
+yum install -y https://dl.grafana.com/oss/release/grafana-6.4.1-1.x86_64.rpm
+
+# Start service Grafana
+service grafana-server start
+
+#
+/sbin/chkconfig -add grafana-server
+
+#  start service automatically at boot 
+systemctl enable grafana-server
+
+# list many plugins
+grafana-cli plugins list-remote
+
+# Install plugin to zabbix in grafana
+grafana-cli plugins install alexanderzobnin-zabbix-app
+
+# Restart service Grafana
+service grafana-server restart
