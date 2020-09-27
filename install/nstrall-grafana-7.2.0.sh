105ea966d70b5479aa6c29652fe792a3c9649b78
Douglas Esteves <douglas.brsoftware@gmail.com>
2020-09-27 16:50:54 -0300

    Create nstrall-grafana-7.2.0.sh

---
+++ install/nstrall-grafana-7.2.0.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Install Grafana 7.2.0
+# Author:      Douglas Esteves 
+# date:        27-September-2020
+
+# CentOS 7/8 & Redhat
+yum install -y https://dl.grafana.com/oss/release/grafana-7.2.0-1.x86_64.rpm
+
+# Start service Grafana
+service grafana-server start
+
+#
+/sbin/chkconfig -add grafana-server
+
+#  start service automatically at boot 
+systemctl enable grafana-server
+
+# list many plugins
+grafana-cli plugins list-remote
+
+# Install plugin to zabbix in grafana
+grafana-cli plugins install alexanderzobnin-zabbix-app
+
+# Restart service Grafana
+service grafana-server restart
